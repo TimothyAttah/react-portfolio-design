82307080d71312712e2f7b4e89e7a296cd3fed58
Timothy Attah <timothyattah733@gmail.com>
2021-10-29 12:00:33 +0100

    Created services page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import { About } from './pages/about/About';
 import { AllPages } from './pages/AllPages';
 import { Home } from './pages/home/Home';
+import { Service } from './pages/service/Service';
 
 export const App = () => {
   return (
@@ -13,11 +14,20 @@ export const App = () => {
 					<Route exact path='/'>
 						<AllPages />
             <Home />
-            <About />
+						<About />
+						<Service />
+					</Route>
+					<Route exact path='/home'>
+						<AllPages />
+						<Home />
 					</Route>
 					<Route exact path='/about'>
 						<AllPages />
 						<About />
+					</Route>
+					<Route exact path='/services'>
+						<AllPages />
+						<Service />
 					</Route>
 				</div>
 			</Switch>
