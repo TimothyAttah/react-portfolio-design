33d72124af2ac3ee8e4eae2f0112f95cafbacbb7
Timothy Attah <timothyattah733@gmail.com>
2021-11-03 18:35:16 +0100

    added styleswitcherstyles.js

--- client/src/components/styleSwitcher/StyleSwitcher.jsx
+++ client/src/components/styleSwitcher/StyleSwitcher.jsx
@@ -1,113 +1,20 @@
 import React, { useEffect, useState } from 'react'
 import { Cloud, NightsStay, Settings } from '@material-ui/icons';
-import styled, { css } from 'styled-components';
 
-export const StyleSwitcherContainer = styled.div`
-	position: fixed;
-	right: 0;
-	top: 77px;
-	width: 280px;
-	z-index: 1999;
-	transition: all 0.3s ease;
-	h4 {
-		margin: 0 0 10px;
-		color: var(--text-black-700);
-		font-size: 1rem;
-		font-weight: 600;
-		text-transform: capitalize;
-	}
-`;
-
-export const StyleSwitcherWrapper = styled.div`
-	display: flex;
-	justify-content: space-between;
-	position: relative;
-`;
-
-export const IconWrapper = styled.div`
-	display: flex;
-	flex-direction: column;
-	justify-content: space-between;
-	align-items: center;
-	position: absolute;
-	right: 0;
-`;
-
-export const ColorsWrapper = styled.div`
-	width: 200px;
-	padding: 15px;
-	border-radius: 5px;
-	transition: all 0.3s ease;
-	margin-right: 20px;
-`;
-
-
-export const StyleToggler = styled.div`
-	margin-right: 15px;
-	height: 40px;
-	width: 40px;
-	text-align: center;
-	font-size: 1.4rem;
-	color: var(--text-black-900);
-	cursor: pointer;
-	transition: all 0.3s ease;
-	border-radius: 50%;
-	display: flex;
-	align-items: center;
-	justify-content: center;
-	:last-child {
-		margin-top: 10px;
-	}
-	::after {
-		border-radius: 50%;
-	}
-	.MuiSvgIcon-root {
-		animation: spin 2s linear infinite;
-	}
-	${props =>
-		props.primary &&
-		css`
-			.MuiSvgIcon-root {
-				animation: none;
-			}
-		`}
-`;
-
-export const Colors = styled.div`
-	display: flex;
-	flex-wrap: wrap;
-	align-items: center;
-	justify-content: space-between;
-
-	span {
-		display: inline-block;
-		height: 30px;
-		width: 30px;
-		border-radius: 50%;
-		cursor: pointer;
-	}
-	.color-1 {
-		background-color: #fb839e;
-	}
-	.color-2 {
-		background-color: #ec9412;
-	}
-	.color-3 {
-		background-color: #1fc586;
-	}
-	.color-4 {
-		background-color: #2eb1ed;
-	}
-	.color-5 {
-		background-color: #cc3a3b;
-	}
-`;
-
+import {
+	StyleSwitcherContainer,
+	ColorsWrapper,
+	StyleSwitcherWrapper,
+	Colors,
+	IconWrapper,
+	StyleToggler
+} from './StyleSwitcherStyles';
 
 export const StyleSwitcher = () => {
   const [ open, setOpen ] = useState( false );
   const [ night, setNight ] = useState( true );
 	const alternateStyle = document.querySelectorAll( '.alternate-style' );
+
 	const setActiveStyle = ( color ) => {
 		localStorage.setItem( 'color', color )
 		changeColorTheme();
@@ -122,7 +29,6 @@ export const StyleSwitcher = () => {
 			setOpen(false);
 		});
 	};
-
 	 const handleNightDay = () => {
 			setNight(!night);
 			document.body.classList.toggle('dark');
@@ -132,7 +38,6 @@ export const StyleSwitcher = () => {
 				localStorage.setItem('theme', 'light');
 			}
 		};
-
 		const changeColor = () => {
 			if (localStorage.getItem('theme') !== null) {
 				if (localStorage.getItem('theme') === 'light') {
@@ -149,7 +54,7 @@ export const StyleSwitcher = () => {
 			changeColorTheme();
 		}
 		changeColor()
-	}, [] );
+	} ,[]);
 
   const handleOpen = () => {
     setTimeout( () => {
@@ -157,29 +62,14 @@ export const StyleSwitcher = () => {
     },300)
   }
 
- 
-
   useEffect( () => {
     window.addEventListener( 'scroll', () => {
       setOpen(false)
     } )
   }, [] )
 
-  useEffect( () => {
-    // window.addEventListener( 'load', () => {
-    //   if ( document.body.classList.contains( 'dark' ) ) {
-    //      setNight(false)
-    //   }
-    //   else {
-    //     setNight(true)
-    //   }
-		// } )
-		// handleNightDay()
-  })
-  
-
   return (
-    <StyleSwitcherContainer className="style-switcher">
+    <StyleSwitcherContainer>
 			<StyleSwitcherWrapper>
 				{open && (
 					<ColorsWrapper className='outer-shadow'>
@@ -211,17 +101,16 @@ export const StyleSwitcher = () => {
 				<IconWrapper>
 					<StyleToggler
 						onClick={handleOpen}
-						className='style-toggler s-icon outer-shadow hover-in-shadow'
+						className='outer-shadow hover-in-shadow'
 					>
 						<Settings />
 					</StyleToggler>
 					<StyleToggler
             primary
             onClick={handleNightDay}
-						className='day-night s-icon outer-shadow hover-in-shadow'
+						className='outer-shadow hover-in-shadow'
           >
             {night ? <NightsStay /> : <Cloud />}
-						
 					</StyleToggler>
 				</IconWrapper>
 			</StyleSwitcherWrapper>
