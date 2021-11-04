4ceabc02daf782be1ff6a42c4158afab64bca099
Timothy Attah <timothyattah733@gmail.com>
2021-11-04 09:58:15 +0100

    change icon

--- client/src/components/styleSwitcher/StyleSwitcher.jsx
+++ client/src/components/styleSwitcher/StyleSwitcher.jsx
@@ -111,7 +111,7 @@ export const StyleSwitcher = () => {
             onClick={handleNightDay}
 						className='outer-shadow hover-in-shadow'
           >
-            {night ? <Brightness7 /> : <Brightness4 />}
+            {night ? <Brightness4 /> : <Brightness7 />}
 					</StyleToggler>
 				</IconWrapper>
 			</StyleSwitcherWrapper>
