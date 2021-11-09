fb722e4dd9f5df9f4c04b00022b96b70cb94f9dc
Timothy Attah <timothyattah733@gmail.com>
2021-11-09 11:50:21 +0100

    created contact me with email.js dep

--- client/src/pages/contact/Contact.jsx
+++ client/src/pages/contact/Contact.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Wrapper, Container, SectionTitle } from '../../styles/globalStyles';
 import { Email, Home, Phone } from '@material-ui/icons';
+import emailjs from 'emailjs-com';
 
 import {
 	ContactItemInner,
@@ -11,8 +12,45 @@ import {
 	InputGroup,
 	SubmitBtn
 } from './ContactStyles';
+import { useState } from 'react';
+
+const Result = () => {
+	return (
+		<h3>
+			Your message has been successfully sent.
+			I will contact you soon.
+		</h3>
+	)
+}
 
 export const Contact = () => {
+	const [ result, showResult ] = useState( true );
+	 const sendEmail = e => {
+			e.preventDefault();
+
+			emailjs
+				.sendForm(
+					'service_dnl2eqd',
+					'template_7ec2pal',
+					e.target,
+					'user_XLI6JUsxKhaq9PWPaKuMv'
+				)
+				.then(
+					result => {
+						console.log(result.text);
+					},
+					error => {
+						console.log(error.text);
+					}
+		 );
+		 e.target.reset();
+		 showResult( true );
+	};
+	
+	setTimeout( () => {
+		showResult(false)
+	}, 5000)
+
   return (
 		<ContactSection>
 			<Container>
@@ -46,12 +84,13 @@ export const Contact = () => {
 				</Wrapper>
 				<Wrapper>
 					<ContactForm>
-						<Form>
+						<Form onSubmit={sendEmail}>
 							<Wrapper>
 								<div className='w-50'>
 									<InputGroup className='input-group outer-shadow hover-in-shadow'>
 										<input
 											type='text'
+											name='name'
 											placeholder='Name'
 											className='input-control'
 										/>
@@ -59,6 +98,7 @@ export const Contact = () => {
 									<InputGroup className='input-group outer-shadow hover-in-shadow'>
 										<input
 											type='email'
+											name='email'
 											placeholder='Email'
 											className='input-control'
 										/>
@@ -66,6 +106,7 @@ export const Contact = () => {
 									<InputGroup className='input-group outer-shadow hover-in-shadow'>
 										<input
 											type='text'
+											name='subject'
 											placeholder='Subject'
 											className='input-control'
 										/>
@@ -74,6 +115,7 @@ export const Contact = () => {
 								<div className='w-50'>
 									<InputGroup className='input-group outer-shadow hover-in-shadow'>
 										<textarea
+											name='message'
 											placeholder='Message...'
 											className='input-control'
 										></textarea>
@@ -83,7 +125,10 @@ export const Contact = () => {
               <Wrapper>
                 <SubmitBtn>
                   <button className="outer-shadow hover-in-shadow">Send Message</button>
-                </SubmitBtn>
+								</SubmitBtn>
+								<div>
+									{result ? <Result /> : null}
+								</div>
               </Wrapper>
 						</Form>
 					</ContactForm>
